ae4d97e5174ca1baba6689aed5e5db5d2dee384f
Evlima <limaemanuel06@gmail.com>
2024-01-15 23:07:19 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// o qque esta sendo feito aqui é: 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
